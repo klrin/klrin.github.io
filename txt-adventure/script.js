0acc969f2f76c1d992dfd6c3ed174b7dcd0e6705
klrin <admin@leafpte.org>
2021-06-14 23:34:26 -0700

    Added lame game functionality.

--- txt-adventure/script.js
+++ txt-adventure/script.js
@@ -71,11 +71,64 @@ function dragElement(elmnt) {
 }
 
 //Storyline-----------------------------------------
+class Item {
+  constructor(name, description, varname) {
+    this.name = name;
+    this.description = description;
+    this.varname = varname;
+    things[this.name] = varname;
+  }
+  read() {
+    describe(this.description);
+  }
+}
+
+class Room {
+  constructor(name, description, objects) {
+    this.name = name;
+    this.description = description;
+    this.objects = objects;
+  }
+}
+
+const things = {
+  "":""
+};
+
+//Rooms---
+var currentroom = "entryway";
+let entryway = new Room("The Entryway", "You are in a drab, cell like room with no doors. There is a window on the east wall and a plaque on the west wall.", ["plaque", "window", "wall", "floor", "bug", "myself"]);
+
+
+//Things--
+let plaque = new Item("plaque", "The plaque reads: I'm too lazy to make a good adventure.", "plaque");
+let thewindow = new Item("window", "You peer out the window, finding a strange stream of 1's and 0's. You think you can make out the word error.", "thewindow");
+let thewall = new Item("wall", "The wall is boring.", "thewall");
+let thefloor = new Item("floor", "The floor is boring, apart from a bug crawling across it.", "thefloor");
+let thebug = new Item("bug", "The bug is green, but you also notice 1's and 0's flowing out of it's shell.", "thebug");
+let theself = new Item("myself", "You are a bug.", "theself");
+console.log(eval(currentroom).name);
+function look(arg) {
+  console.log(eval(currentroom).name);
+  if (arg == "") {
+    describe(eval(currentroom).name);
+    describe(eval(currentroom).description);
+  } else {
+    if (eval(currentroom).objects.includes(arg)) {
+      describe(eval(things[arg]).description);
+    } else {
+      describe("You see no "+arg+" of interest.");
+    }
+  }
+}
 
 function respond() {
   if (inputval == "") {
     describe("Time passes...");
-  } else {
-    describe(inputval);
+  } else if(inputval.includes("look")) {
+    var item = inputval.replace("look", "").replace(" at ", "");
+    look(item);
   }
 }
+
+// TODO: 
