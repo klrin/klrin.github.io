98691f5896c5460d775a7421e178ae0b1dfba6eb
klrin <admin@leafpte.org>
2021-06-21 20:51:44 -0700

    Update

--- txt-adventure/script.js
+++ txt-adventure/script.js
@@ -1,15 +1,26 @@
-var inputval = "";
-var turns = 0;
 
-function txsubmit(e) {
+// Variables:
+
+let inputval = "", turns = 0, saveindex = 0, currentroom = "entryway", lastcommand = ["", "", "", "", "", "", "", "", "", ""];
+
+
+
+//--------------------------------------------------
+//--------------------USER-INTERFACE----------------
+//--------------------------------------------------
+
+
+
+function txsubmit(e) { //What happens when the input is submitted
   e.preventDefault();
   try {
-    var logger = document.getElementsByClassName('body')[0];
-    inputval = document.getElementById('txt-entry').value;
+    let log = document.getElementsByClassName('body')[0],
+    txtbox = document.getElementById('txt-entry');
+    inputval = txtbox.value;
     entry(inputval);
     respond();
-    logger.scrollTop = logger.scrollHeight;
-    document.getElementById('txt-entry').value = "";
+    log.scrollTop = log.scrollHeight;
+    txtbox.value = "";
     turns += 1;
     document.getElementById('turns').innerHTML = "Turns: "+turns;
   } catch (e) {
@@ -18,15 +29,32 @@ function txsubmit(e) {
   return false;
 }
 
-function entry(text) {
+function entry(text) { // For user input
   document.getElementsByClassName('body')[0].innerHTML += "<p class='entry'>>>>"+text+"</p>";
 }
-function describe(text) {
+function describe(text) { // For computer-generated text
   document.getElementsByClassName('body')[0].innerHTML += "<p class='description'>"+text+"</p>";
 }
 
+// ----History for text box with up and down arrows
 
-//Make the DIV element draggagle:
+function history(e) {
+  e = e || window.event;
+  if (e.keyCode == '38') { // up arrow
+    if (saveindex > 0) { saveindex -= 1; }
+    document.getElementById('txt-entry').value = lastcommand[saveindex];
+    e.preventDefault();
+    e.preventDefault();
+  } else if (e.keyCode == '40') { // down arrow
+    if (saveindex < 10) { saveindex += 1; }
+    if (saveindex == 10) { document.getElementById('txt-entry').value = ""; }
+    else { document.getElementById('txt-entry').value = lastcommand[saveindex]; }
+    e.preventDefault();
+  }
+}
+
+//---Make the DIV element draggagle:
+
 dragElement(document.getElementById("mydiv"));
 
 function dragElement(elmnt) {
@@ -70,70 +98,62 @@ function dragElement(elmnt) {
   }
 }
 
-//-----------
+
+//--------------------------------------------------
+//--------------------STORYLINE---------------------
+//--------------------------------------------------
 
 
-
-
-//Storyline-----------------------------------------
-
-
-
-//---------
-class Item {
-  constructor(name, description, varname) {
+class Item { //---Classes
+  constructor(name, description) {
     this.name = name;
     this.description = description;
-    this.varname = varname;
-    things[this.name] = varname;
-  }
-  read() {
-    describe(this.description);
   }
 }
 
 class Room {
-  constructor(name, description, objects) {
+  constructor(name, description, objects, x, y) {
     this.name = name;
     this.description = description;
     this.objects = objects;
+    this.x = x;
+    this.y = y;
     this.features = {room: this.description};
   }
-
   feature(name, description) {
-    this.features[name] = description;
+    if (typeof(name)=="string") {
+      this.features[name] = description;
+    } else {
+      for (let i = 0; i < name.length; i++) {
+        this.features[name[i]] = description;
+      }
+    }
   }
 }
 
 
 
-//Rooms---
-var currentroom = "entryway";
+// Define Rooms---
 
-let entryway = new Room("The Entryway", "You are in a drab, cell like room with no doors. There is a window on the east wall and a plaque on the west wall.", ["plaque", "window", "wall", "floor", "bug", "myself"]);
-
-entryway.feature("plaque", "The plaque reads: I'm too lazy to make a good adventure.");
-entryway.feature("window", "You peer out the window, finding a strange stream of 1's and 0's. You think you can make out the word error.");
+//Entryway
+let entryway = new Room("The Entryway", "You are in a drab, cell like room with no doors. There is a window on the east wall and a plaque on the west wall.", ["knife"]);
+//features
+entryway.feature(["plaque", "sign", "westwall", "west", "w"], "The plaque reads: I'm too lazy to make a good adventure.");
+entryway.feature(["window", "eastwall", "east", "e"], "You peer out the window, finding a strange stream of 1's and 0's. You think you can make out the word error.");
 entryway.feature("wall", "The wall is boring.");
 entryway.feature("floor", "The floor is boring, apart from a bug crawling across it.");
-entryway.feature("ground", "The ground is boring, apart from a bug crawling across it.");
+entryway.feature(["ground", "floor"] , "The ground is boring, apart from a bug crawling across it.");
 entryway.feature("bug", "The bug is green, but you also notice 1's and 0's flowing out of it's shell.");
 entryway.feature("shell", "The shell is dull.");
 entryway.feature("self", "You are a bug.");
 entryway.feature("error", "It hurts your eyes to look at the error.");
-entryway.feature("1", "You try to focus on it, but you lose it the stream of numbers.");
-entryway.feature("0", "You try to focus on oit, but you lose it the stream of numbers.");
-entryway.feature("one", "You try to focus on it, but you lose it the stream of numbers.");
-entryway.feature("zero", "You try to focus on oit, but you lose it the stream of numbers.");
+entryway.feature(["1", "one", "0", "zero"], "You lose the number in the stream of digits.");
 entryway.feature("cell", "AUOAUND!! -- You think you have a spazm.");
-
-
 //Things--
 
-
+let knife = new Item("Knife", "A sharp metal blade fixed to a worn handle.");
 
 function look(arg) {
-  console.log("lokking inside "+eval(currentroom).name);
   if (arg == "") {
     describe(eval(currentroom).name+":");
     describe(eval(currentroom).description);
@@ -141,35 +161,35 @@ function look(arg) {
     if (arg in eval(currentroom).features) {
       describe(eval(currentroom).features[arg]);
     } else {
-      console.log("lokking at "+arg);
       describe("You see no "+arg+" of interest.");
     }
   }
 }
 
-var common = ["a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your", "ain't", "aren't", "can't", "could've", "couldn't", "didn't", "doesn't", "don't", "hasn't", "he'd", "he'll", "he's", "how'd", "how'll", "how's", "i'd", "i'll", "i'm", "i've", "isn't", "it's", "might've", "mightn't", "must've", "mustn't", "shan't", "she'd", "she'll", "she's", "should've", "shouldn't", "that'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "wasn't", "we'd", "we'll", "we're", "weren't", "what'd", "what's", "when'd", "when'll", "when's", "where'd", "where'll", "where's", "who'd", "who'll", "who's", "why'd", "why'll", "why's", "won't", "would've", "wouldn't", "you'd", "you'll", "you're", "you've", "'s"];
-var space = ["  "];
-var lookwords = ["look", "examine"];
+var common = ["a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your", "ain't", "aren't", "can't", "could've", "couldn't", "didn't", "doesn't", "don't", "hasn't", "he'd", "he'll", "he's", "how'd", "how'll", "how's", "i'd", "i'll", "i'm", "i've", "isn't", "it's", "might've", "mightn't", "must've", "mustn't", "shan't", "she'd", "she'll", "she's", "should've", "shouldn't", "that'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "wasn't", "we'd", "we'll", "we're", "weren't", "what'd", "what's", "when'd", "when'll", "when's", "where'd", "where'll", "where's", "who'd", "who'll", "who's", "why'd", "why'll", "why's", "won't", "would've", "wouldn't", "you'd", "you'll", "you're", "you've"];
+
+var lookwords = ["look", "examine", "l"];
 function sanitize(txt, list) {
 	  var expStr = list.join("|");
 	  return txt.replace(new RegExp('\\b(' + expStr + ')\\b', 'gi'), ' ')
                     .replace(/\s{2,}/g, ' ');
-  }
+}
+
 
 
 function respond() {
+  //Command History
+  lastcommand.push(inputval);
+  if (lastcommand.length > 10) { lastcommand.shift(); }
+  saveindex = lastcommand.length;
+
+  // Sanitize inputval
+  inputval = sanitize(inputval, common);
   console.log(inputval);
-  inputval = sanitize(inputval, common).toLowerCase();
+  inputval = inputval.toLowerCase().split(" ");
   console.log(inputval);
-  if (inputval == "") {
-    describe("Time passes...");
-  } else if(inputval.includes("look") || inputval.includes("examine")) {
-    look(sanitize(inputval, lookwords).replaceAll(" ", ""));
-  } else if(inputval.includes("suck")) {
-    describe("<img src='https://purepng.com/public/uploads/medium/big-chungus-z7y.png'");
-  } else {
-    describe("Sorry, I don't understand.");
-  }
+
+
 }
 
 // TODO:
